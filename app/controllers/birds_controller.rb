f5e15644e1bce3042f983d457bb57b36ee450095
steve <austines811@gmail.com>
2022-09-26 18:24:48 +0300

    commit tree 83251be1dd72e417de47a7c2487915bce083d0a4

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
 
   # GET /birds
   def index
@@ -14,6 +15,15 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+  # POST a bird
+  def create
+    bird = Bird.create(birds_params)
+    render json: bird, status: :created
+  end
+  #private method to access anly listed params
+  def birds_params
+    params.permit(:name, :species)
   end
 
 end
